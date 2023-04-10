97e39ea9df64dbd3880beaef4c87b01e88c1adc5
George <g.sarinakis@gmail.com>
2023-04-10 15:37:13 +0300

    change on .gitingore on /.idea
    Major app changes 3:
    
    --> New design on Registration
    
    --> Added real time firebase database
    
    --> Functionalities:
        i) Checks if an email is already signed in:
    
            --> if it is then takes the user to LogInActivity
            --> if it is not then registers the email with an ID
    
        ii) It still has Toasts reporting on everything, canceled procedures, successful procedures, etc.
    
        iii) Checks for empty user input form as well as incorrect matching on passwords.
    
    --> Added a data class for user creation on the database

--- app/src/main/java/com/example/qrecycle/RegisterActivity.kt
+++ app/src/main/java/com/example/qrecycle/RegisterActivity.kt
@@ -3,47 +3,137 @@ package com.example.qrecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
-import android.view.View
+import android.text.TextUtils
+import android.view.WindowManager
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
+
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var emailText: EditText
-    private lateinit var email: String
+    private lateinit var database: FirebaseDatabase
+    private lateinit var usersRef: DatabaseReference
+
+    //TODO: Add Google, Facebook and Instagram log in options
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("DEPRECATION")
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_register)
 
-        val registerBtn : Button = findViewById(R.id.registerBtn)
-        val emailAddress : TextView = findViewById(R.id.etEmail)
+        FirebaseApp.initializeApp(this)
+        database = FirebaseDatabase.getInstance()
+        usersRef = database.reference.child("users")
 
-        emailText = emailAddress as EditText
-        email = emailText.text.toString().trim()
+        val registerBtn : Button= findViewById(R.id.registerBtn)
+
+        val fullNameInput        : TextInputLayout = findViewById(R.id.editTextFullName)
+        val emailInput           : TextInputLayout = findViewById(R.id.editTextEmail)
+        val passwordInput        : TextInputLayout = findViewById(R.id.editTextPassword)
+        val confirmPasswordInput : TextInputLayout = findViewById(R.id.editTextConfirmPassword)
 
         registerBtn.setOnClickListener {
-            val intent = Intent(this, DashBoardActivity::class.java)
-            startActivity(intent)
-            //checkEmailAddress(emailAddress, intent)
-            //finish()
+            registerUser(fullNameInput, emailInput, passwordInput, confirmPasswordInput)
         }
-
     }
 
-    private fun checkEmailAddress(view: View, intent: Intent) {
-        val text1 = emailText.text
+    private fun registerUser(
+        name: TextInputLayout,
+        email : TextInputLayout,
+        password : TextInputLayout,
+        confirmPassword : TextInputLayout
+    ){
+        val nameStr            = name.editText?.text.toString()
+        val emailStr           = email.editText?.text.toString().trim {it <= ' '}
+        val passwordStr        = password.editText?.text.toString().trim {it <= ' '}
+        val confirmPasswordStr = confirmPassword.editText?.text.toString().trim {it <= ' '}
 
-        if(text1.isEmpty()) {
-            Toast.makeText(this,"Please enter your email address",Toast.LENGTH_SHORT).show()
-        } else {
-            if (Patterns.EMAIL_ADDRESS.matcher(text1).matches()) {
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+        if (validateForm(nameStr, emailStr, passwordStr, confirmPasswordStr)) {
+            val ref = FirebaseDatabase.getInstance().getReference("users")
+            ref.orderByChild("email").equalTo(emailStr).addListenerForSingleValueEvent(
+                object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        startLogInActivity()
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Email already registered",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    } else {
+                        val id = ref.push().key
+                        val user = User(id, nameStr, emailStr, passwordStr)
+
+                        if (id != null) {
+                            ref.child(id)
+                                .setValue(user)
+                                .addOnSuccessListener {
+                                    Toast.makeText(
+                                        this@RegisterActivity,
+                                        "You have successfully registered to QRecycle",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    startDashboardActivity()
+                                    finish()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(
+                                        this@RegisterActivity,
+                                        "Registration failed",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Error checking email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+        }
+    }
+
+    private fun startDashboardActivity() {
+        val intent = Intent(this, DashBoardActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startLogInActivity() {
+        val intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun validateForm(name : String, email : String, password : String, confirmPassword : String) : Boolean{
+        return when {
+            TextUtils.isEmpty(name) -> {
+                Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show()
+                false
             }
+            TextUtils.isEmpty(email) -> {
+                Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+                false
+            }
+            password != confirmPassword -> {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                false
+            }
+            else -> true
         }
     }
 }
