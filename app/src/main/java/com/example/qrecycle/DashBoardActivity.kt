319b590d3a0e8ab336c518b8b40103daee9f1bda
George <g.sarinakis@gmail.com>
2023-07-04 19:41:12 +0300

    change on .gitingore on /.idea
    Changes on Gradle.build app and root dir
    DashBoard map added :)
    
    changes on all other activities

--- app/src/main/java/com/example/qrecycle/DashBoardActivity.kt
+++ app/src/main/java/com/example/qrecycle/DashBoardActivity.kt
@@ -1,13 +1,33 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.qrecycle
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.WindowManager
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 class DashBoardActivity : AppCompatActivity() {
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 123
+    private val INITIAL_ZOOM_LEVEL = 14.0
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("DEPRECATION")
@@ -21,6 +41,79 @@ class DashBoardActivity : AppCompatActivity() {
 
         profileBtn.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        // Initialize the map view
+        mapView = findViewById(R.id.mapView)
+        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        mapView.controller.setZoom(INITIAL_ZOOM_LEVEL)
+        mapView.setMultiTouchControls(true)
+
+        // Request location permission
+        requestLocationPermission()
+    }
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            // Permission already granted, start retrieving location
+            retrieveLocation()
+        }
+    }
+
+    private fun retrieveLocation() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                val userLatitude = location.latitude
+                val userLongitude = location.longitude
+
+                // Center the map on the user's location
+                val userLocation = GeoPoint(userLatitude, userLongitude)
+                mapView.controller.setCenter(userLocation)
+            }
+
+            @Deprecated("Deprecated in Java")
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener, null)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Location permission granted
+            retrieveLocation()
+        } else {
+            Toast.makeText(
+                this@DashBoardActivity,
+                "Location access denied",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
